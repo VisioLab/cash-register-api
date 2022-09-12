a185d4a2250ea9d8e8b6b469ac4c325d154ccb2f
rbusche <rbusche@posteo.net>
2022-09-12 12:14:20 +0200

    Restructure

--- example-client/src/generateTypes.ts
+++ example-client/src/generateTypes.ts
@@ -6,7 +6,7 @@ import { compile } from 'json-schema-to-typescript'
 
 
 const main = async (): Promise<void> => {
-    const asyncApiDocument = await fs.readFile("../websocket/api.yml")
+    const asyncApiDocument = await fs.readFile("../api.yml")
     const parsedDoc = await parser.parse(asyncApiDocument.toString());
     const types = await Promise.all(
         [...parsedDoc.allMessages().entries()].map(([name, message]) =>
--- rest/README.md
+++
@@ -1,7 +0,0 @@
-# Cash Register Integration Interface Spec (REST)
-
-Describes a REST API, where VisioLab acts as a client.
-
-    pip install -r requirements.txt
-
-    uvicorn api:app --reload
